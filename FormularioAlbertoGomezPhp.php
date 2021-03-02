ba8176a89d4ffbf6084152325b9973fb97cda0ab
Alberto Gomez <albertogomezmorillas@gmail.com>
2021-03-02 18:56:03 +0100

    No me funciona el php

---
+++ FormularioAlbertoGomezPhp.php
@@ -0,0 +1,16 @@
+<html>
+<html lang = 'es'>
+    <head>
+        <meta charset="utf-8">
+    </head>
+
+    <body>
+       <?php
+
+       user = $_POST['nick'];
+       echo user;
+       contr = $_POST['pass'];
+       echo contr;
+       ?>
+    </body>
+</html>
